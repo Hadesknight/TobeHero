7f80bb51957e3bebea39def3f24b54585b12b435
Hadesknight <fernandorato.0@hotmail.com>
2020-03-24 16:59:07 -0300

    Adicionado Migrations

--- Backend/src/routes.js
+++ Backend/src/routes.js
@@ -2,7 +2,11 @@ const express = require('express')
 
 const routes = express.Router()
 
+const OngController = require('./Controllers/OngController')
 
+
+
+routes.post('/ongs', OngController.store)
 
 
 module.exports = routes
