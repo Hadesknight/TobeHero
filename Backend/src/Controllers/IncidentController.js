1912a19a7916da66aa5b5203f5218ce40f38741c
Hadesknight <fernandorato.0@hotmail.com>
2020-03-24 18:38:08 -0300

    ADicionado paginação

--- Backend/src/Controllers/IncidentController.js
+++ Backend/src/Controllers/IncidentController.js
@@ -6,6 +6,8 @@ module.exports = {
     const  {title, description, value} = req.body
     const ong_id = req.headers.authorization
 
+
+
     const [id] = await connection('incidents').insert({
       title, description, value, ong_id
     })
@@ -15,10 +17,18 @@ module.exports = {
   },
 
   async index(req, res){
+    const {page = 1} = req.query;
 
-    const incidents = await connection('incidents').select('*')
+    const [count] = await connection('incidents').count()
 
+    console.log(count)
 
+    const incidents = await connection('incidents')
+      .limit(5)
+      .offset((page -1) * 5)
+      .select('*')
+
+    res.header('X-Total-Count', count['count(*)'])
     return res.json(incidents)
   },
 
