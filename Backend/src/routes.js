a26cc593725f458914a450c5d0384d437c35e677
Hadesknight <fernandorato.0@hotmail.com>
2020-03-24 18:20:37 -0300

    Adicionado listagem de incidente por Ongs

--- Backend/src/routes.js
+++ Backend/src/routes.js
@@ -4,6 +4,7 @@ const routes = express.Router()
 
 const OngController = require('./Controllers/OngController')
 const IncidentController = require('./Controllers/IncidentController')
+const ProfileController = require('./Controllers/ProfileController')
 
 
 routes.post('/ongs', OngController.store)
@@ -13,5 +14,7 @@ routes.get('/ongs', OngController.index)
 routes.post('/incidents', IncidentController.store)
 routes.get('/incidents', IncidentController.index)
 routes.delete('/incidents/:id', IncidentController.delete)
+
+routes.get('/profile', ProfileController.index)
 
 module.exports = routes
