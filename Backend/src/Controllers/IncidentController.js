f2322e76b7a6deeffbd71d97494e0f2d3155f9ae
Hadesknight <fernandorato.0@hotmail.com>
2020-03-24 18:43:28 -0300

    Adicionado Join

--- Backend/src/Controllers/IncidentController.js
+++ Backend/src/Controllers/IncidentController.js
@@ -21,12 +21,20 @@ module.exports = {
 
     const [count] = await connection('incidents').count()
 
-    console.log(count)
+    
 
     const incidents = await connection('incidents')
+      .join('ongs', 'ong_id', '=', 'incidents.ong_id')
       .limit(5)
       .offset((page -1) * 5)
-      .select('*')
+      .select([
+        'incidents.*', 
+        'ongs.name', 
+        'ongs.email', 
+        'ongs.whatsapp', 
+        'ongs.city', 
+        'ongs.uf'
+      ])
 
     res.header('X-Total-Count', count['count(*)'])
     return res.json(incidents)
