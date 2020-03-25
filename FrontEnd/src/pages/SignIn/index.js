09a41a961c7ae20b8de675b5b5ae4018aacd99f1
Hadesknight <fernandorato.0@hotmail.com>
2020-03-25 14:58:15 -0300

    SignIn Creado

--- FrontEnd/src/pages/SignIn/index.js
+++ FrontEnd/src/pages/SignIn/index.js
@@ -1,6 +1,8 @@
-import React from 'react';
-import {Link} from 'react-router-dom'
+import React, {useState} from 'react';
+import {Link, useHistory} from 'react-router-dom'
 import {FiLogIn} from 'react-icons/fi'
+
+import api from '../../services/api'
 
 import './styles.css'
 
@@ -8,15 +10,34 @@ import heroesImg  from '../../assets/heroes.png'
 import logoImg from '../../assets/logo.svg'
 
 export default function SignIn() {
+
+  const [id, setId] = useState('')
+
+  const history = useHistory()
+  async function handleSubmit(e){
+    e.preventDefault()
+    console.log("teste")
+
+   try{
+    const response = await api.post('session', {id})
+    localStorage.setItem('ongId', id)
+    localStorage.setItem('ongName', response.data.name)
+
+    history.push('/profile')
+   }catch(err){
+     alert("Falha no login")
+   }
+    
+  }
   return (
     <div className="logon-container">
       <section className="form">
         <img src={logoImg} alt="logo" />
 
-        <form>
+        <form onSubmit={handleSubmit}>
           <h1>Faça seu Logon</h1>
 
-          <input placeholder="Sua ID"/>
+          <input placeholder="Sua ID" value={id} onChange={e => setId(e.target.value)}/>
           <button type="submit" className="button">Entrar</button>
 
 
