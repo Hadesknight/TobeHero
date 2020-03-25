e7e73e6012b91556d451433a198e7fb7644b41a1
Hadesknight <fernandorato.0@hotmail.com>
2020-03-25 12:39:16 -0300

    Criado pagina de login

--- FrontEnd/src/pages/SignIn/index.js
+++ FrontEnd/src/pages/SignIn/index.js
@@ -1,9 +1,35 @@
 import React from 'react';
 
+import {FiLogIn} from 'react-icons/fi'
+
 import './styles.css'
+
+import heroesImg  from '../../assets/heroes.png'
+import logoImg from '../../assets/logo.svg'
 
 export default function SignIn() {
   return (
-    <h1 >Login</h1>
+    <div className="logon-container">
+      <section className="form">
+        <img src={logoImg} alt="logo" />
+
+        <form>
+          <h1>Faça seu Logon</h1>
+
+          <input placeholder="Sua ID"/>
+          <button type="submit" className="button">Entrar</button>
+
+
+          <a href="/register">
+          <FiLogIn size={16} color ="#e02041"/>
+            Não Tenho Cadastro
+          </a>
+        </form>
+
+
+      </section>
+
+      <img src={heroesImg} alt="heroes"/>
+    </div>
   );
 }
