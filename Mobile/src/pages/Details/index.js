ec2165432b91bdd00e115a62e38dd334a9360c21
Hadesknight <fernandorato.0@hotmail.com>
2020-03-26 14:42:45 -0300

    Adicionado Stack NavigatioN

---
+++ Mobile/src/pages/Details/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View , Text} from 'react-native';
+
+// import { Container } from './styles';
+
+export default function Details() {
+  return (
+    <View><Text>Details</Text></View>
+  );
+}
---
+++ Mobile/src/pages/Incidents/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+// import { Container } from './styles';
+
+export default function Incidents() {
+  return (
+    <View><Text>Incidents</Text></View>
+  );
+}
